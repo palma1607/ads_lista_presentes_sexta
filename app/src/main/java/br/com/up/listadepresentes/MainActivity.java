d52891723157c342ff338f30bf030a46cc16a220
gilbriatore <geucimar@gmail.com>
2022-09-23 22:37:15 -0300

    Aula 23/09

--- app/src/main/java/br/com/up/listadepresentes/MainActivity.java
+++ app/src/main/java/br/com/up/listadepresentes/MainActivity.java
@@ -1,6 +1,9 @@
 package br.com.up.listadepresentes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,18 +13,30 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
+import br.com.up.listadepresentes.adapters.GiftAdapter;
 import br.com.up.listadepresentes.models.Gift;
 import br.com.up.listadepresentes.repositories.GiftRepository;
 
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton fabAddGift;
+    private RecyclerView recyclerViewGifts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewGifts = findViewById(R.id.recycler_gift);
         fabAddGift = findViewById(R.id.fab_add_gift);
+
+        recyclerViewGifts.setLayoutManager(
+                new LinearLayoutManager(this,
+                        RecyclerView.VERTICAL,
+                        false
+                        )
+               // new GridLayoutManager(this,8)
+        );
 
         fabAddGift.setOnClickListener(
                 new View.OnClickListener(){
@@ -49,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         if(gifts.size() > 0){
 
         }
+
+        recyclerViewGifts.setAdapter(new GiftAdapter(gifts));
 
     }
 }
